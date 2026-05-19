a0ac2b5d6b12b8f7d3833552b8550ad5272c73fd
infosecdairies01 <infosecdairies@gmail.com>
2026-05-19 19:43:40 +0530

    Configure logo.png as global browser tab favicon icon

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -9,6 +9,7 @@ import {
 } from "@tanstack/react-router";
 
 import appCss from "../styles.css?url";
+import logoUrl from "../assets/logo.png?url";
 
 function NotFoundComponent() {
   return (
@@ -85,6 +86,11 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
       {
         rel: "stylesheet",
         href: appCss,
+      },
+      {
+        rel: "icon",
+        type: "image/png",
+        href: logoUrl,
       },
     ],
   }),
